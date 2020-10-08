6393a6566432a6fa9b6c7bdbe0e3a84a40993adb
gctev <gadgetscare.com@gmail.com>
2020-10-08 17:13:55 +0530

    first push

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,8 @@ import React from 'react'
  const About = () => {
     return (
         <div>
-            <h1>ABout</h1>
-            <p>how you doin??</p>
+            <h1>About</h1>
+            <p>Search github users....</p>
         </div>
     )
 }
